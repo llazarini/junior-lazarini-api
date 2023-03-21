5cc0b6f77e412cd482fbf73b97ec02bb01a027fb
Leonardo Lazarini <llazarini2@gmail.com>
2023-03-21 18:55:05 +0000

    Fuels

---
+++ database/migrations/1656445704954_create_fuels_table.ts
@@ -0,0 +1,24 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class extends BaseSchema {
+    protected tableName = 'fuels'
+
+    public async up () {
+        this.schema.createTable(this.tableName, (table) => {
+            table.increments('id')
+            table.string('name');
+            table.string('name');
+            table.string('image');
+            /**
+             * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
+             */
+            table.timestamp('created_at', { useTz: true })
+            table.timestamp('updated_at', { useTz: true })
+            table.dateTime(`deleted_at`, { useTz: true }).defaultTo(null)
+        })
+    }
+
+    public async down () {
+        this.schema.dropTable(this.tableName)
+    }
+}
