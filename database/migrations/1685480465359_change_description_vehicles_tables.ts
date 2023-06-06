8bf318bd553fe9bfe549b41187cc8312d3fdaf41
Leonardo Lazarini <llazarini2@gmail.com>
2023-06-06 23:16:30 +0100

    Change desc.

--- database/migrations/1685480465359_change_description_vehicles_tables.ts
+++ database/migrations/1685480465359_change_description_vehicles_tables.ts
@@ -5,7 +5,7 @@ export default class extends BaseSchema {
 
     public async up () {
         this.schema.alterTable(this.tableName, (table) => {
-            table.string('description').notNullable().alter()
+            table.text('description').notNullable().alter()
         })
     }
 
