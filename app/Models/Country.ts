89f6fe83a093a2a840b1835e4dcc59f395d48d12
Leonardo Lazarini <leonardo.l@konect.ai>
2023-08-03 19:02:04 +0100

    Country image added.

--- app/Models/Country.ts
+++ app/Models/Country.ts
@@ -1,5 +1,6 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+import { BaseModel, column, computed } from '@ioc:Adonis/Lucid/Orm'
+import Env from '@ioc:Adonis/Core/Env'
 
 export default class Country extends BaseModel {
     @column({ isPrimary: true })
@@ -13,10 +14,22 @@ export default class Country extends BaseModel {
 
     @column()
     public countryCode: string
+
+    @column()
+    public image: string
     
     @column.dateTime({ autoCreate: true })
     public createdAt: DateTime
 
     @column.dateTime({ autoCreate: true, autoUpdate: true })
     public updatedAt: DateTime
+
+    /**
+     * Get the video out URL
+     */
+    @computed()
+    public get image_url() {
+        return `${Env.get('S3_URL')}${this.image}`;
+    }
+    
 }
